dd1d3fd91ca1b574257d165407537c7034541b87
harshalkaigaonkar <harshalkaigaonkar31@gmail.com>
2022-01-04 20:22:28 +0530

    changed reply for execution of /prepare including <#>

--- commands/prepare.js
+++ commands/prepare.js
@@ -1,15 +1,21 @@
 const { Permissions } = require('discord.js');
 const { SlashCommandBuilder } = require('@discordjs/builders');
 // const { createJob } = require('../cronjobs/cronJobs');
-const wait = require('util').promisify(setTimeout);
+// const wait = require('util').promisify(setTimeout);
 
 module.exports = {
-	data: new SlashCommandBuilder().setName('prepare').setDescription('Prepares HackBot for the Hackathon'),
+	data: new SlashCommandBuilder()
+		.setName('prepare')
+		.setDescription('Prepares HackBot for the Hackathon')
+		.addStringOption(option => option.setName('unique-code').setDescription('Enter the Unique Code').setRequired(true)),
 	async execute(interaction, client) {
-		await interaction.deferReply();
+		await interaction.deferReply({ ephemeral: true });
+		const id = await interaction.options.getString('unique-code');
 		const everyoneRole = await interaction.member.guild.roles.everyone;
+		// check if DB contains this Hackathon Registered into DB with id
 		// get Hackathon Name from DB
-		await interaction.guild.channels.create('#Hackathon Name', {
+		const hackathonName = 'Hackathon - Manager' + id.toString();
+		const newChannel = await interaction.guild.channels.create(`${hackathonName}`, {
 			type: 0,
 			permissionOverwrites: [
 				{ type: 'member', id: interaction.user.id, allow: [Permissions.FLAGS.VIEW_CHANNEL, Permissions.FLAGS.SEND_MESSAGES] },
@@ -17,8 +23,6 @@ module.exports = {
 				{ type: 'role', id: everyoneRole.id, deny: [Permissions.FLAGS.VIEW_CHANNEL] },
 			],
 		});
-		await interaction.editReply({ content: '' });
-		await wait(4000);
-		await interaction.deleteReply();
+		await interaction.editReply({ content: `Please goto <#${newChannel.id}> for Further details.` });
 	},
 };
